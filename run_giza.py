ed0aaea876cce3c3612d333010f35e9fe06e90f2
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2014-10-06 22:13:42 -0400

    added space removal and 's in English

--- run_giza.py
+++ run_giza.py
@@ -10,6 +10,8 @@ if len(sys.argv)<4:
 def clean_corpus(cleaner_script_path, file_path,src_lang_type,trgt_lang_type,min_num,max_num):
 	clean_path=file_path+'.clean'
 	os.system('perl '+cleaner_script_path+' '+file_path+' '+src_lang_type+' '+trgt_lang_type+' '+clean_path+' '+str(min_num)+' '+str(max_num))
+	os.system('sed -i \'s/  */\ /g\' '+clean_path+'.'+src_lang_type)
+	os.system('sed -i \'s/  */\ /g\' '+clean_path+'.'+trgt_lang_type)
 
 giza_bin_dir=os.path.abspath(sys.argv[1])+'/'
 tokenizer_script_path=os.path.abspath(sys.argv[2])
@@ -34,6 +36,10 @@ os.chdir(dir_path)
 
 print '(MSR_MESSAGE) tokenizing files...'
 sys.stdout.flush()
+if src_lang_type=='en':
+	os.system(' sed -i "s/\' s /\'s /g" '+dir_path+'src.txt')
+if trgt_lang_type=='en':
+	os.system(' sed -i "s/\' s /\'s /g" '+dir_path+'dst.txt')
 os.system('perl '+tokenizer_script_path+' -l '+src_lang_type+' < '+dir_path+'src.txt > '+dir_path+'corpus.tok.'+src_lang_type)
 os.system('perl '+tokenizer_script_path+' -l '+trgt_lang_type+' < '+dir_path+'dst.txt > '+dir_path+'corpus.tok.'+trgt_lang_type)
 
